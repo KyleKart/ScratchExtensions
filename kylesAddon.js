40cc91dcd8679239b281ae9e8fcdef04e3a4766a
KyleKart <71104484+KyleKart@users.noreply.github.com>
2022-02-21 23:38:33 -0400

    Add files via upload

--- kylesAddon.js
+++ kylesAddon.js
@@ -16,12 +16,38 @@ class kylesAddon {
               "defaultValue": "https://reqbin.com/echo/get/json",
             }
           }
+        },  '---',  {
+             opcode: 'test',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'idk [test]',
+            "arguments": {
+              "test": {
+                "type": Scratch.ArgumentType.STRING,
+                "defaultValue": "hello",
+              }
+            }
         },
       ]
     }  
   }
 get({url}) {
     return fetch("https://api.allorigins.win/raw?url=" + url).then(response => response.text()).catch(err => 'ERROR');
+  };
+  test(url) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url);
+    
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    
+    var data = `{ "username":"Funky Fred",
+  "avatar_url":"https://cdn.discordapp.com/attachments/876953412497969169/891302670348976189/download_14.png", "content":"{{Author}} just posted this on r/{{Subreddit}}!", "embeds":[{ "title" : "{{Title}}", "description": " {{Content}}", "author": { "name": "{{Author}}" }, "url" : "{{PostURL}}", "image": {
+      "url": "{{ImageURL}}"
+}
+}]
+}`;
+    
+    xhr.send(data);
   };
 }
 Scratch.extensions.register(new kylesAddon());
