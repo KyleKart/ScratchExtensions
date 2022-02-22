86df1da2200b8a5033b814077dab5268228808c6
KyleKart <71104484+KyleKart@users.noreply.github.com>
2022-02-21 21:50:02 -0400

    Add files via upload

---
+++ kylesAddon.js
@@ -0,0 +1,27 @@
+class kylesAddon {
+  getInfo() {
+    return {
+      id: 'kyleaddon',
+      name: 'Kyle\'sAdoon',
+      color1: '#ff8100',
+      color2: '#ff8100',
+      blocks: [
+        {
+          opcode: 'get',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'get [url]',
+          "arguments": {
+            "url": {
+              "type": Scratch.ArgumentType.STRING,
+              "defaultValue": "https://reqbin.com/echo/get/json",
+            }
+          }
+        },
+      ]
+    }  
+  }
+get({url}) {
+    return fetch("https://api.allorigins.win/raw?url=" + url).then(response => response.text()).catch(err => 'ERROR');
+  };
+}
+Scratch.extensions.register(new kylesAddon());
