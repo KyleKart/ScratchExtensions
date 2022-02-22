f7a13aefb4e3b25d20dd66d158d7b2d66cc52bd8
KyleKart <71104484+KyleKart@users.noreply.github.com>
2022-02-22 00:48:25 -0400

    Add files via upload

--- kylesAddon.js
+++ kylesAddon.js
@@ -28,23 +28,18 @@ get({url}) {
     return fetch("https://api.allorigins.win/raw?url=" + url).then(response => response.text()).catch(err => 'ERROR');
   };
   test() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://discord.com/api/webhooks/945524710047895573/krPI5xZVFIrhZTlIUW-dJgx5y3dob4G2COPPr7b3bTIzYyz7kjFuukZf60T9NhSvg1pe");
-    
-    xhr.setRequestHeader("Accept", "application/json");
-    xhr.setRequestHeader("Content-Type", "application/json");
-    
-    var data = `{ "username":"Funky Fred",
-  "{
-    "username":"@{{UserName}}",
-    "icon_url":"<url to image>",
-    "content":"{{LinkToTweet}}"
-  }"
-}
-}]
-}`;
-    
-    xhr.send(data);
-  };
+    fetch('https://discord.com/api/webhooks/945524710047895573/krPI5xZVFIrhZTlIUW-dJgx5y3dob4G2COPPr7b3bTIzYyz7kjFuukZf60T9NhSvg1pe', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({str: {
+            "username":"name",
+            "content":"message"
+        }})
+      }).then(res => res.json())
+        .then(res => console.log(res));
+};
 }
 Scratch.extensions.register(new kylesAddon());
