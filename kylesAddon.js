3c3e31b79cad7fa26942beabd18a45fc7a68c88f
KyleKart <71104484+KyleKart@users.noreply.github.com>
2022-02-22 08:57:01 -0400

    Update kylesAddon.js

--- kylesAddon.js
+++ kylesAddon.js
@@ -14,31 +14,10 @@ class kylesAddon {
             "url": {
               "type": Scratch.ArgumentType.STRING,
               "defaultValue": "https://reqbin.com/echo/get/json",
-            }
-          }
-        },  '---',  {
-             opcode: 'test',
-            blockType: Scratch.BlockType.COMMAND,
-            text: 'idk',
-        },
-      ]
-    }  
-  }
+            },
+            
 get({url}) {
     return fetch("https://api.allorigins.win/raw?url=" + url).then(response => response.text()).catch(err => 'ERROR');
   };
-  test() {
-    return fetch('https://discord.com/api/webhooks/ooga/booga', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({str: {
-            "username":"name",
-            "content":"message"
-        }})
-      }).then(res => res.json())
-        .then(res => console.log(res));
-};
 }
 Scratch.extensions.register(new kylesAddon());
