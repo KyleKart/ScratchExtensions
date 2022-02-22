0c10ff2c16336698099334d5256f54ff6344712b
KyleKart <71104484+KyleKart@users.noreply.github.com>
2022-02-21 23:39:42 -0400

    Add files via upload

--- kylesAddon.js
+++ kylesAddon.js
@@ -33,8 +33,21 @@ class kylesAddon {
 get({url}) {
     return fetch("https://api.allorigins.win/raw?url=" + url).then(response => response.text()).catch(err => 'ERROR');
   };
-  test(test) {
-    return alert(test);
+  test(url) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url);
+    
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    
+    var data = `{ "username":"Funky Fred",
+  "avatar_url":"https://cdn.discordapp.com/attachments/876953412497969169/891302670348976189/download_14.png", "content":"{{Author}} just posted this on r/{{Subreddit}}!", "embeds":[{ "title" : "{{Title}}", "description": " {{Content}}", "author": { "name": "{{Author}}" }, "url" : "{{PostURL}}", "image": {
+      "url": "{{ImageURL}}"
+}
+}]
+}`;
+    
+    xhr.send(data);
   };
 }
 Scratch.extensions.register(new kylesAddon());
