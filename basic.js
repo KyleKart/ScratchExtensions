ddacee964c5bf043c35a490b4ce218abc8617d3f
KyleKart <71104484+KyleKart@users.noreply.github.com>
2023-03-13 10:42:10 -0300

    Add files via upload

--- basic.js
+++ basic.js
@@ -32,10 +32,20 @@ menus: {
 };
     }
   tf({input1, input2}) {
-    if (input1 == input2) {
-        return true
+    var text1 = input1
+    var text2 = input2
+    console.log("Before: " + text1);
+    console.log("Before: " + text2);
+    if (input1 == true) var text1 = "true";
+    if (input1 == false) var text1 = "false";
+    if (input2 == true) var text2 = "true";
+    if (input2 == false) var text2 = "false";
+    console.log("After: " + text1);
+    console.log("After: " + text2);
+    if (text1 == text2) {
+        return "true"
     } else
-    return false
+    return "false"
 };
 }
 Scratch.extensions.register(new BasicK());
