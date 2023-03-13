f039e6c4f81e7455cc04a54b607981bf382bd3b6
KyleKart <71104484+KyleKart@users.noreply.github.com>
2023-03-12 21:22:06 -0300

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,64 @@
+class BasicK {
+    getInfo() {
+      return {
+        id: 'basick',
+        name: 'Basic_K',    
+      
+   blocks: [
+        {
+          opcode: 'get',
+          blockType: Scratch.BlockType.BOOLEAN,
+          text: '[input1] = [input2]',
+          "arguments": {
+            "input1": {
+              "type": Scratch.ArgumentType.STRING,
+              "defaultValue": "true",
+              "menu": "embed",
+            },
+            "input2": {
+                "type": "string",
+                "defaultValue": 'false',
+                "menu": "embed",
+            },
+          }
+        },  '---',  {
+             opcode: 'test',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'SEND | Message:[message] URL:[url] Embed:[embed]',
+            "arguments": {
+                "message": {
+                    "type": "string",
+                    "defaultValue": "Hello!",
+                },
+                "url": {
+                    "type": "string",
+                    "defaultValue": 'https://discord.com/api/webhooks/ID/TOKEN',
+
+                },
+                "true": {
+                    "type": "string",
+                    "defaultValue": 'false',
+                    "menu": "embed",
+                },
+            }
+        },
+],
+menus: {
+    embed: {
+        acceptReporters: true,
+        items: [{ text: "true", value: "true"}, {text: "false", value: "false"}]
+    }
+}
+};
+}
+get({url}) {
+    return fetch("https://api.allorigins.win/raw?url=" + url).then(response => response.text()).catch(err => 'ERROR');
+  };
+  test({input1, input2}) {
+    if (input1 == input2){
+        return "true"
+    } else
+    return "false"
+};
+}
+Scratch.extensions.register(new BasicK());
