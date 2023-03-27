4c9111b1d10016bea3d3684810b22abcc9347cde
KyleKart <71104484+KyleKart@users.noreply.github.com>
2023-03-27 15:32:30 -0300

    Add files via upload

---
+++ cursed.js
@@ -0,0 +1,33 @@
+class cursedBlocks {
+    getInfo() {
+      return {
+        id: 'cursedblocks',
+        name: 'C U R S E D',    
+
+   blocks: [
+        {
+          opcode: 'touchie',
+          blockType: Scratch.BlockType.BOOLEAN,
+          text: 'touchie to [input1] ?',
+          "arguments": {
+            "input1": {
+              "type": "string",
+              "defaultValue": "true",
+              "menu": "options1",
+            },
+          }
+        },
+],
+menus: {
+    options1: {
+        acceptReporters: true,
+        items: [{ text: " ", value: " "}]
+    }, 
+} 
+};
+    }
+  touchie({input1}) {
+
+};
+}
+Scratch.extensions.register(new BasicK());
