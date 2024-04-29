f4d395cfe50e66bd37259a8b34e1e4abe119b841
KyleKart <71104484+KyleKart@users.noreply.github.com>
2024-04-29 05:03:36 -0300

    gfghg

--- get/helloworld-x.js
+++ get/helloworld-x.js
@@ -1,17 +1,14 @@
 (function(ext) {
-    // Define a block to output "Hello, world!"
+    
     ext.sayHello = function(callback) {
-        // Invoke the callback with the string "Hello, world!"
         callback("Hello, world!");
     };
 
-    // Block and block menu descriptions
     var descriptor = {
         blocks: [
             ['R', 'say hello', 'sayHello']
         ]
     };
 
-    // Register the extension
     ScratchExtensions.register('Hello World Extension', descriptor, ext);
 })({});
