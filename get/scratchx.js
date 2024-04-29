9310083d3652a7bb6044bfe983c0947cc0c4746a
KyleKart <71104484+KyleKart@users.noreply.github.com>
2024-04-29 04:56:27 -0300

    new

--- get/scratchx.js
+++ get/scratchx.js
@@ -1,34 +1,17 @@
 (function(ext) {
-    // Function to replace every word in a string with "Joe"
-    function replaceWordsWithJoe(html) {
-        // Split the string into words
-        var words = html.split(/\s+/);
-        
-        // Replace each word with "Joe"
-        for (var i = 0; i < words.length; i++) {
-            words[i] = 'Joe';
-        }
-        
-        // Join the words back into a string
-        return words.join(' ');
-    }
-
-    // Define a block to replace every word in HTML with "Joe"
-    ext.replaceWordsWithJoe = function(html, callback) {
-        // Replace words in the HTML
-        var newHtml = replaceWordsWithJoe(html);
-        
-        // Invoke the callback with the manipulated HTML
-        callback(newHtml);
+    // Define a block to output "Hello, world!"
+    ext.sayHello = function(callback) {
+        // Invoke the callback with the string "Hello, world!"
+        callback("Hello, world!");
     };
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            ['R', 'replace every word in HTML %s with "Joe"', 'replaceWordsWithJoe', '<p>Hello, world!</p>']
+            ['R', 'say hello', 'sayHello']
         ]
     };
 
     // Register the extension
-    ScratchExtensions.register('Joe HTML Extension', descriptor, ext);
+    ScratchExtensions.register('Hello World Extension', descriptor, ext);
 })({});
