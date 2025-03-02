54cf2cff1b1c729df6adb25f3b39e57f2dfbce90
KyleKart <71104484+KyleKart@users.noreply.github.com>
2025-03-02 16:36:47 -0400

    extensofsa

---
+++ get/clipboardx.js
@@ -0,0 +1,59 @@
+(function (ext) {
+
+  ext.whenCopied = function() {
+    if (copiedBool) {
+      copiedBool = false;
+      return true;
+    }
+    return false;
+  };
+
+  ext.whenPasted = function() {
+    if (pastedBool) {
+      pastedBool = false;
+      return true;
+    }
+    return false;
+  };
+
+  ext.setClipboard = function(text) {
+    navigator.clipboard.writeText(text);
+  };
+
+  ext.resetClipboard = function() {
+    navigator.clipboard.writeText("");
+  };
+
+  ext.clipboard = function() {
+    if (navigator.clipboard && navigator.clipboard.readText) {
+      return JSON.stringify(navigator.clipboard.readText()) ?? "";
+    }
+    return "";
+  };
+
+  ext.canClipboard = function() {
+    return navigator.clipboard;
+  };
+
+  ext.getLastPastedText = function() {
+    return lastPastedText;
+  };
+
+  var blocks = [
+    ['h', 'when something is copied', 'whenCopied'],
+    ['h', 'when something is pasted', 'whenPasted'],
+    [' '],
+    ['', 'copy to clipboard: %s', 'setClipboard', ''],
+    ['', 'reset clipboard', 'resetClipboard', ''],
+    [' '],
+    ['r', 'clipboard', 'clipboard', ''],
+    ['r', 'last pasted text', 'getLastPastedText', ''],
+    ['b', 'clipboard?', 'canClipboard']
+  ];
+
+  var descriptor = {
+    blocks: blocks,
+  };
+
+  ScratchExtensions.register('clipboard', descriptor, ext);
+})({});
