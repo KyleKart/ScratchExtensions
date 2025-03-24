3a629bb1a1467d492c3f43e6fd271feabf125759
KyleKart <71104484+KyleKart@users.noreply.github.com>
2025-03-24 12:57:20 -0300

    ddd

--- get/titlebar.js
+++ get/titlebar.js
@@ -1,26 +1,26 @@
 (function (Scratch) {
     'use strict';
 
-const myStyles = `
-display: flex;
-flex-direction: row;
-align-items: center;
-gap: 0rem;
-`;
-const element = document.querySelector('.menu-bar_file-group_1_CHX');
+  const myStyles = `
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 0rem;
+  `;
 
-element.style.cssText = myStyles;
+  const element = [...document.querySelectorAll('[class*="menu-bar_file-group"]')][0];
+  if (!element) return;
 
-var items = [...element.children];
+  element.style.cssText = myStyles;
 
-const newOrder = [1,2,3,4,5,0]
+  var items = [...element.children];
+  const newOrder = [1,2,3,4,5,0];
 
-items.sort((a, b)=>newOrder.indexOf(items.indexOf(a)) - newOrder.indexOf(items.indexOf(b)));
+  items.sort((a, b) => newOrder.indexOf(items.indexOf(a)) - newOrder.indexOf(items.indexOf(b)));
+  items.forEach(it => element.appendChild(it));
 
-items.forEach(it=>element.appendChild(it));
-
-const element2 = document.querySelectorAll('.menu-bar_menu-bar-item_oLDa-')[3];
-element2.remove();
+  const element2 = [...document.querySelectorAll('[class*="menu-bar_menu-bar-item"]')][3];
+  if (element2) element2.remove();
 
   class TitleBar {
       getInfo() {
